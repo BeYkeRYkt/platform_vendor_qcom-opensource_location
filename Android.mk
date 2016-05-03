de2acd2e129164983969abd049df6be70555fa72
Roopesh Rajashekharaiah Nataraja <roopes@codeaurora.org>
2016-05-03 13:07:13 -0700

    location: Enable location modules
    Change-Id: I8ad78d6d0bfd8d666c47d0ebf08cc27cebf965fb

--- Android.mk
+++ Android.mk
@@ -1,8 +1,5 @@
-#FIXME: Disabled for Feldspar bringup
-ifneq ($(BUILD_FELDSPAR_PDK),true)
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
 endif
