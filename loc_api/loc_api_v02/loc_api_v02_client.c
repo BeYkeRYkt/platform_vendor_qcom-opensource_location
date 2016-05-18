f038c4e65476f22670a003db1b43cf7459b2d7b5
Saurabh Srivastava <ssrivast@codeaurora.org>
2016-05-19 01:30:26 +0530

    Adding size argument for loc_sync_process_ind()
    Adding a new paylod size argument for payload pointer being passed
    in to loc_sync_process_ind() API
    
    CRs-Fixed: 983163
    Signed-off-by: Sivanesan Rajapupathi <srajap@codeaurora.org>
    
    Conflicts:
    	loc_api/loc_api_v02/loc_api_sync_req.c
    	loc_api/loc_api_v02/loc_api_v02_client.c
    	loc_api/loc_api_v02/loc_api_v02_client.h
    
    Change-Id: Iedf5a75639e95affc9dd312b5babfe59afeb3542

--- loc_api/loc_api_v02/loc_api_v02_client.c
+++ loc_api/loc_api_v02/loc_api_v02_client.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1488,20 +1488,21 @@ static void locClientIndCb
           respIndUnion.pDeleteAssistDataInd =
             (qmiLocDeleteAssistDataIndMsgT_v02 *)indBuffer;
 
-          /* call the response callback
-           * To avoid calling the respCallback after locClientClose
-           * is called, check pCallbackData->respCallback again here
-           */
-          if((NULL != localRespCallback) &&
-              (NULL != pCallbackData->respCallback))
-          {
-            localRespCallback(
-                (locClientHandleType)pCallbackData,
-                msg_id,
-                respIndUnion,
-                pCallbackData->pClientCookie);
-          }
-        }
+        /* call the response callback
+         * To avoid calling the respCallback after locClientClose
+         * is called, check pCallbackData->respCallback again here
+         */
+        if((NULL != localRespCallback) &&
+           (NULL != pCallbackData->respCallback))
+        {
+          localRespCallback(
+              (locClientHandleType)pCallbackData,
+              msg_id,
+              respIndUnion,
+              indSize,
+              pCallbackData->pClientCookie);
+		}
+       }
       }
       else // error handling indication
       {
