LineageOS/android_device_essential_mata

XTRA Client 2.0
This project implemented XTRA "Enhancements" grouped as XTRA Client 2.0.
1. Added new QMI apis for PCID injection
2. Added new QMI api for XTRA data injection

CRs-fixed: 917950
Change-Id: I5542a4a5dc9d711ac4bdc4fc4da8b8a22355697f

loc_api/loc_api_v02/loc_api_v02_client.c
@@ -615,7 +615,14 @@ static const locClientRespIndTableStructT locClientRespIndTable[]= {
 
    //Delete Gnss Service Data Resp Ind
    { QMI_LOC_DELETE_GNSS_SERVICE_DATA_IND_V02,
-     sizeof(qmiLocDeleteGNSSServiceDataIndMsgT_v02) }
+     sizeof(qmiLocDeleteGNSSServiceDataIndMsgT_v02) },
+
+   // for XTRA Client 2.0
+   { QMI_LOC_INJECT_XTRA_DATA_IND_V02,
+     sizeof(qmiLocInjectXtraDataIndMsgT_v02) },
+
+   { QMI_LOC_INJECT_XTRA_PCID_IND_V02,
+     sizeof(qmiLocInjectXtraPcidIndMsgT_v02) }
 };
 
 
@@ -1516,6 +1523,19 @@ static bool validateRequest(
     {
       *pOutLen = sizeof(qmiLocDeleteGNSSServiceDataReqMsgT_v02);
       break;
+    }
+
+    // XTRA Client 2.0
+    case QMI_LOC_INJECT_XTRA_DATA_REQ_V02:
+    {
+        *pOutLen = sizeof(qmiLocInjectXtraDataReqMsgT_v02);
+        break;
+    }
+
+    case QMI_LOC_INJECT_XTRA_PCID_REQ_V02:
+    {
+        *pOutLen = sizeof(qmiLocInjectXtraPcidReqMsgT_v02);
+        break;
     }
 
     // ALL requests with no payload

BeYkeRYkt/platform_vendor_qcom-opensource_location

loc_api/loc_api_v02/loc_api_v02_client.c
@@ -611,8 +611,14 @@ static const locClientRespIndTableStructT locClientRespIndTable[]= {
     sizeof(qmiLocGdtReceiveDoneStatusIndMsgT_v02) },
 
    { QMI_LOC_GDT_DOWNLOAD_END_STATUS_IND_V02,
-     sizeof(qmiLocGdtDownloadEndStatusIndMsgT_v02) }
+     sizeof(qmiLocGdtDownloadEndStatusIndMsgT_v02) },
 
+   // for XTRA Client 2.0
+   { QMI_LOC_INJECT_XTRA_DATA_IND_V02,
+     sizeof(qmiLocInjectXtraDataIndMsgT_v02) },
+
+   { QMI_LOC_INJECT_XTRA_PCID_IND_V02,
+     sizeof(qmiLocInjectXtraPcidIndMsgT_v02) }
 };
 
 
@@ -1507,6 +1513,19 @@ static bool validateRequest(
     case QMI_LOC_GDT_DOWNLOAD_END_STATUS_REQ_V02:
     {
         *pOutLen = sizeof(qmiLocGdtDownloadEndStatusReqMsgT_v02);
+        break;
+    }
+
+    // XTRA Client 2.0
+    case QMI_LOC_INJECT_XTRA_DATA_REQ_V02:
+    {
+        *pOutLen = sizeof(qmiLocInjectXtraDataReqMsgT_v02);
+        break;
+    }
+
+    case QMI_LOC_INJECT_XTRA_PCID_REQ_V02:
+    {
+        *pOutLen = sizeof(qmiLocInjectXtraPcidReqMsgT_v02);
         break;
     }
 
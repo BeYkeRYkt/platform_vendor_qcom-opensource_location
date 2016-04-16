29ab5077a37c97f6bc290ada3e3dddb8391e5f2f
Ruifeng Xu <ruifeng@codeaurora.org>
2016-04-15 17:33:30 -0700

    XTRA Client 2.0
    This project implemented XTRA "Enhancements" grouped as XTRA Client 2.0.
    1. Added new QMI apis for PCID injection
    2. Added new QMI api for XTRA data injection
    
    CRs-fixed: 917950
    Change-Id: I5542a4a5dc9d711ac4bdc4fc4da8b8a22355697f

--- loc_api/loc_api_v02/loc_api_v02_log.c
+++ loc_api/loc_api_v02/loc_api_v02_log.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -338,7 +338,13 @@ static const loc_name_val_s_type loc_v02_event_name[] =
     NAME_VAL(QMI_LOC_GDT_DOWNLOAD_END_STATUS_IND_V02),
     NAME_VAL(QMI_LOC_EVENT_GDT_DOWNLOAD_BEGIN_REQ_IND_V02),
     NAME_VAL(QMI_LOC_EVENT_GDT_RECEIVE_DONE_IND_V02),
-    NAME_VAL(QMI_LOC_EVENT_GDT_DOWNLOAD_END_REQ_IND_V02)
+    NAME_VAL(QMI_LOC_EVENT_GDT_DOWNLOAD_END_REQ_IND_V02),
+    NAME_VAL(QMI_LOC_INJECT_XTRA_DATA_REQ_V02),
+    NAME_VAL(QMI_LOC_INJECT_XTRA_DATA_RESP_V02),
+    NAME_VAL(QMI_LOC_INJECT_XTRA_DATA_IND_V02),
+    NAME_VAL(QMI_LOC_INJECT_XTRA_PCID_REQ_V02),
+    NAME_VAL(QMI_LOC_INJECT_XTRA_PCID_RESP_V02),
+    NAME_VAL(QMI_LOC_INJECT_XTRA_PCID_IND_V02)
 };
 static const int loc_v02_event_num = sizeof(loc_v02_event_name) / sizeof(loc_name_val_s_type);
 
