LineageOS/android_device_essential_mata

LPPe feature support.
1. QMI LOC IDL changes for LPPe feature
2. Additional logic to get the supported
versions for  ODCPI and WiFi data injection
QMI messages

CRs-Fixed: 974312

Change-Id: I9c84ca51ff0b3857d372336a632a1a45ccbc77e6

loc_api/loc_api_v02/loc_api_v02_log.c
@@ -344,7 +344,10 @@ static const loc_name_val_s_type loc_v02_event_name[] =
     NAME_VAL(QMI_LOC_INJECT_XTRA_DATA_IND_V02),
     NAME_VAL(QMI_LOC_INJECT_XTRA_PCID_REQ_V02),
     NAME_VAL(QMI_LOC_INJECT_XTRA_PCID_RESP_V02),
-    NAME_VAL(QMI_LOC_INJECT_XTRA_PCID_IND_V02)
+    NAME_VAL(QMI_LOC_INJECT_XTRA_PCID_IND_V02),
+    NAME_VAL(QMI_LOC_GET_SUPPORTED_FEATURE_REQ_V02),
+    NAME_VAL(QMI_LOC_GET_SUPPORTED_FEATURE_RESP_V02),
+    NAME_VAL(QMI_LOC_GET_SUPPORTED_FEATURE_IND_V02),
 };
 static const int loc_v02_event_num = sizeof(loc_v02_event_name) / sizeof(loc_name_val_s_type);
 

BeYkeRYkt/platform_vendor_qcom-opensource_location

loc_api/loc_api_v02/loc_api_v02_log.c
@@ -338,7 +338,10 @@ static const loc_name_val_s_type loc_v02_event_name[] =
     NAME_VAL(QMI_LOC_GDT_DOWNLOAD_END_STATUS_IND_V02),
     NAME_VAL(QMI_LOC_EVENT_GDT_DOWNLOAD_BEGIN_REQ_IND_V02),
     NAME_VAL(QMI_LOC_EVENT_GDT_RECEIVE_DONE_IND_V02),
-    NAME_VAL(QMI_LOC_EVENT_GDT_DOWNLOAD_END_REQ_IND_V02)
+    NAME_VAL(QMI_LOC_EVENT_GDT_DOWNLOAD_END_REQ_IND_V02),
+    NAME_VAL(QMI_LOC_GET_SUPPORTED_FEATURE_REQ_V02),
+    NAME_VAL(QMI_LOC_GET_SUPPORTED_FEATURE_RESP_V02),
+    NAME_VAL(QMI_LOC_GET_SUPPORTED_FEATURE_IND_V02),
 };
 static const int loc_v02_event_num = sizeof(loc_v02_event_name) / sizeof(loc_name_val_s_type);
 